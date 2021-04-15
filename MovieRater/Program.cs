ddf6b378bc027e64cbc81a588cc6a93e55e6c746
Trejon-Hughes <trejonhughes15@gmail.com>
2021-04-15 09:08:21 -0400

    Add project files.

---
+++ MovieRater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieRater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
