5cb511b7d058f5bfeedb938e9d79326611b637b8
ekkaiden <2003kaiden@gmail.com>
2021-04-15 16:57:32 -0400

    kaiden

--- MovieRaterRepo/MovieRaterRepo.cs
+++ MovieRaterRepo/MovieRaterRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieRater;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace MovieRaterRepo
 {
     public class MovieRaterRepo
     {
+        public readonly List<Movie> _movie = new List<Movie>();
+        public bool AddMovie(Movie content)
+        {
+            int startingCount = _movie.Count;
+
+            _movie.Add(content);
+
+            bool wasAdded = (_movie.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+        public bool DeleteMovie(Movie existingMovie)
+        {
+            bool removeMovie = _movie.Remove(existingMovie);
+            return removeMovie;
+        }
+        public List<Movie> GetMovies()
+        {
+            return _movie;
+        }
     }
 }
